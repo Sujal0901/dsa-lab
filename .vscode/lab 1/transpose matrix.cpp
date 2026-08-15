326c4a506753f6be3e7382e71df3bbc05a926bbc
Sujal0901 <sujalrai452@gmail.com>
2026-08-15 19:35:26 +0530

    lab 1

---
+++ .vscode/lab 1/transpose matrix.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    cout << "Transpose:\n";
+
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 2; i++) {
+            cout << a[i][j] << " ";
+        }
+        cout <<endl;
+    }
+
+    return 0;
+}
